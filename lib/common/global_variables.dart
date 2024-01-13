b1e674c050e093861e91075eab9c4b2e97f596c7
Arifian0 <arifdwi1294@gmail.com>
2024-01-14 02:11:09 +0700

    bug global variable

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,4 +1,3 @@
 class GlobalVariables {
-  static const baseUrl =
-      'https://bd4c-2001-448a-4061-3756-2530-6620-51db-5b80.ngrok-free.app';
+  static const baseUrl = 'https://7867-182-253-197-50.ngrok-free.app';
 }
